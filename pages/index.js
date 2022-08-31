f54fe07ad411f6207816df1d1d0e4d929ad9ea7d
Argim <akaliqi@outlook.com>
2022-08-31 18:15:10 +0200

    Pizza-Card/List/Updated Nav-bar

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar';
 import Featured from "../components/Featured";
-
 import styles from '../styles/Home.module.css'
+import PizzaList from '../components/PizzaList';
 
 export default function Home() {
   return (
@@ -15,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured/>
+      <PizzaList/>
       
     </div>
   );
